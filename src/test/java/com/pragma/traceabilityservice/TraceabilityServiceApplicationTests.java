e39200d138be47ed97a97a115ae9362318490232
Johan Pineda <mpineda348@gmail.com>
2023-09-28 12:25:09 -0500

    creating and configuring the project

---
+++ src/test/java/com/pragma/traceabilityservice/TraceabilityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.traceabilityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraceabilityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
